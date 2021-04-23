e06b5b5ffef50a6edd01a1d647e63516f5b7c347
lukasz-kuch <lukas.walbrzych@gmail.com>
2021-04-23 12:48:34 +0200

    code refactoring

--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@ variable "network" {
   description = "Network name"
 }
 
+variable "credentials" {
+  type = string
+  description = "Credential path"
+}
+
 variable "project_id" {
   type = string
   description = "Project ID of project that has App Engine created and proper Service Account configured"
@@ -19,30 +24,19 @@ variable "app_source" {
   description = "A path to the data you want to upload"
 }
 
-variable "bucket_name" {
-  type = string
-  description = "Existing bucket name"
-}
-
-variable "bucket_object_name" {
-  type = string
-  default = "app-engine"
-  description = "New bucket object name"
-}
-
 variable "ip_cidr_range" {
   default = "10.8.0.0/28"
   type = string
   description = "IP range must be an unused /28 CIDR range, such as 10.8.0.0/28. The VPC Connector will use IP addresses from this range. Ensure that the range does not overlap with an existing VPC network's subnets"
 }
 
-variable "version_id" {
+variable "app_version" {
   default = "v1"
   type = string
   description = "App version name"
 }
 
-variable "service_name" {
+variable "app_name" {
   type   = string
   default = "default"
   description = "The first service (module) you upload to a new application must be the 'default' service (module). Please upload a version of the 'default' service (module) before uploading a version for the 'custom' service (module)"
